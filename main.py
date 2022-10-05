856098bbb684020d923788cb81f2a9cde2225763
Samuel Varghese <gamingsam77@gmail.com>
2022-10-05 15:47:21 +0100

    Start of punctuation model

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from typing import Union
-from fastapi import FastAPI
+# from fastapi import FastAPI
 import requests
 
-app = FastAPI()
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+
+tokenizer = AutoTokenizer.from_pretrained("oliverguhr/fullstop-punctuation-multilang-large")
+
+model = AutoModelForTokenClassification.from_pretrained("oliverguhr/fullstop-punctuation-multilang-large")
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
