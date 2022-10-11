8d8d62b6bc1999803eb128194ea18d8356435f11
Samuel Varghese <gamingsam77@gmail.com>
2022-10-11 09:44:23 +0100

    results from batch transcript

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
+from dataclasses import dataclass
+from msvcrt import setmode
 import os
 from typing import Union
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+# from fastapi import FastAPI
+# from fastapi.responses import FileResponse
 import requests
 import whisper
 
 
-app = FastAPI()
+# app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
 
 
 def transcribe(audio_file_path):
@@ -26,15 +28,33 @@ def transcribe(audio_file_path):
 
 
 def open_results(file_path):
-    with open(file_path, "r") as f:
+    with open(file_path, "r", encoding="utf-8") as f:
         text = f.read()
         return text
 
 
 def save_file(file_name, text):
-    with open(file_name, 'w') as f:
+    with open(file_name, 'w', encoding="utf-8") as f:
         f.write(text)
 
+@dataclass
+class Sermon:
+    name: str
+    transcript: str
+
+def batch_transcribe(locations_of_sermons):
+    files = os.listdir(locations_of_sermons)
+    sermons_audios = [file for file in files if file.endswith(".mp3")]
+    print("Starting Transcription...")
+    for sermon_audio in sermons_audios:
+        print("Transcribing Sermon "+ sermon_audio)
+        sermon = Sermon(sermon_audio[:-4], transcribe("mp3/"+sermon_audio))
+        sermon.transcript = "# "+ sermon.name + "\n" + sermon.transcript
+        print("Saving")
+        save_file("results/"+sermon.name+".md", sermon.transcript)
+        print("Save successful")
+
+batch_transcribe("mp3")
 
 def split_into_chunks(long_string: str, per_period: int):
     split_string = long_string.split(".")
